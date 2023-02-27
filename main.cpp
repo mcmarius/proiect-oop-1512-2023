02b85052f271c956e4a2b14cc9be0b70a5aee949
mcmarius <23401453+mcmarius@users.noreply.github.com>
2023-02-27 12:42:26 +0200

    Getteri și setteri

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+class Student {
+    std::string nume;// = "test";
+    int grupa;// = 100;
+public:
+    void setNume(std::string nume_) {
+        nume = nume_;
+    }
+    void setGrupa(int grupa_) {
+        grupa = grupa_;
+    }
+    std::string getNume() { return nume; }
+    int getGrupa() { return grupa; }
+};
+
 int main() {
-    std::cout << "Hello, 1512!\n";
+    Student st1, st2{}, st3{st2}, st4(st3), st5 = st1;//, st6();
+    int a;
+    st1 = st2;
+//    st6();
+//    st1.nume = "test 2";
+    std::cout << st1.getNume() << " " << st1.getGrupa() << "\n";
+    st1.setNume("Test 2");
+    std::cout << st1.getNume() << " " << st1.getGrupa() << "\n";
+    st1.setGrupa(1512);
+    std::cout << st1.getNume() << " " << st1.getGrupa() << "\n";
     return 0;
 }
